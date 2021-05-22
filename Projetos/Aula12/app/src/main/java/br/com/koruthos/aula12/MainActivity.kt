de44d97fdf4bfd7bf08000f6d18bb57ee2c0c5e1
Chaua Queirolo <chaua@users.noreply.github.com>
2021-05-21 22:30:22 -0300

    :tada: Importação inicial

---
+++ Projetos/Aula12/app/src/main/java/br/com/koruthos/aula12/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.koruthos.aula12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
