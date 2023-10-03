d068ab8663b413f3051442a0615ade13c0de15e6
Chaua Queirolo <chaua@users.noreply.github.com>
2023-10-02 22:26:33 -0300

    :sparkles: Exemplos de componentes

---
+++ projetos/CursoAndroid/app/src/main/java/br/com/koruthos/cursoandroid/ComponentesActivity.kt
@@ -0,0 +1,16 @@
+package br.com.koruthos.cursoandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class ComponentesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_componentes)
+    }
+
+
+
+}
