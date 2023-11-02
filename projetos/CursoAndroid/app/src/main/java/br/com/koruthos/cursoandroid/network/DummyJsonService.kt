08feaf6799bc538d22478c27f056fc84c3b15171
Chaua Queirolo <chaua@users.noreply.github.com>
2023-11-01 22:21:53 -0300

    :sparkles: Inclusão dos endpoints

--- projetos/CursoAndroid/app/src/main/java/br/com/koruthos/cursoandroid/network/DummyJsonService.kt
+++ projetos/CursoAndroid/app/src/main/java/br/com/koruthos/cursoandroid/network/DummyJsonService.kt
@@ -1,18 +1,62 @@
 package br.com.koruthos.cursoandroid.network
 
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface DummyJsonService {
 
-//    GET 	/products 	// get all products
-//    GET 	/products/1 	// get single product
-//    GET 	/products/search?q=Laptop 	// search products
-//    GET 	/products/categories 	// get product categories
-//    GET 	/products/category/smartphones 	// get products of a category
-//    POST 	/products/add 	// add a product
-//    PUT 	/products/1 	// update a product
-//    PATCH 	/products/1 	// update a product
-//    DELETE 	/products/1 	// delete a product
-//    GET (auth) 	/auth/products 	// get products with auth token
+    // Neste arquivo são configurados todos os endpoints do servidor no formato de função usando anotações:
+    // - Na declaração de cada função, utiliza o método HTTP do endpoint
+    //      @GET, @POST, @DELETE, @PUT, @PATCH
+    //
+    // - O caminho do endpoint é passado como parâmetro da anotação
+    //      @GET("products/")
+    //
+    // - Se a URL tiver algum parâmetro na URL, ele é definido entre {}.
+    // - Este parâmetro deverá ser declarado como parâmetros do método usando a anotação @Path
+    //      @DELETE("products/{id})
+    //      fun deletar(@Path("id") var): ...
+    //
+    // - Se a URL tiver algum parâmetro opcionalm ele é definido no método com a anotação @Query
+    //
+    // - Se o método precisar enviar um objeto no corpo da mensagem, ele é definido com a anotação @Body
+    // - IMPORTANTE: o objeto deve ser serializável!
+    //
+    // - O método deve retornar um Call (retrofit2) com o tipo dos dados esperados na reposta do endpoint
 
+    // GET 	/products 	        // get all products
+    @GET("products/")
+    fun getTodosProdutos(): Call<Any>
 
+    // GET 	/products/1 	    // get single product
+    @GET("products/{id}")
+    fun getProduto(@Path("id") id: Int): Call<Any>
+
+    // GET 	/products/search?q=Laptop 	// search products
+    @GET("products/")
+    fun getProdutoPorNome(@Query("q") query: String): Call<Any>
+
+    // POST 	/products/add 	// add a product
+    @POST("products/add")
+    fun postProduto(@Body produto: Any): Call<Any>
+
+    // PUT 	/products/1 	    // update a product
+    @PUT("products/{id}")
+    fun putProduto(@Path("id") id: Int, @Body produto: Any): Call<Any>
+
+    // PATCH 	/products/1 	// update a product
+    @PATCH("products/{id}")
+    fun patchProduto(@Path("id") id: Int, @Body produto: Any): Call<Any>
+
+    // DELETE 	/products/1 	// delete a product
+    @DELETE("products/{id}")
+    fun deleteProduto(@Path("id") id: Int): Call<Any>
 
 }
