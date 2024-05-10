a9632c9ecf5e437e240c9ccd10009dc5c2939eb8
Chauã Queirolo <chaua@users.noreply.github.com>
2024-05-10 20:25:25 -0300

    :sparkles: Exemplo de Data Binding

--- projetos/CursoAndroid/app/build.gradle.kts
+++ projetos/CursoAndroid/app/build.gradle.kts
@@ -30,6 +30,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    // 1. Habilitar o databinding
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -38,6 +42,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.databinding:databinding-runtime:8.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- projetos/CursoAndroid/app/src/main/java/br/com/koruthos/cursoandroid/DataBindingActivity.kt
+++ projetos/CursoAndroid/app/src/main/java/br/com/koruthos/cursoandroid/DataBindingActivity.kt
@@ -4,18 +4,19 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import br.com.koruthos.cursoandroid.databinding.ActivityDataBindingBinding
 
-class DataBindingActivity : AppCompatActivity() {
+class DataBindingActivity : AppCompatActivity(), View.OnClickListener {
 
-    @SuppressLint("MissingInflatedId")
+    private lateinit var mBinding: ActivityDataBindingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Informa qual será o layout para ser carregado na Activity
-        setContentView(R.layout.activity_data_binding)
 
         // Para acessar um componente de tela existem
         // várias maneiras
@@ -24,12 +25,24 @@ class DataBindingActivity : AppCompatActivity() {
         // - Módulo de data binding
 
         // 1. Modo tradicional
-        val painel: TextView = findViewById(R.id.databinding_txt_painel)
-        val botao: Button = findViewById(R.id.databinding_btn_oi)
+        // Informa qual será o layout para ser carregado na Activity
+        // setContentView(R.layout.activity_data_binding)
+
+//        val painel: TextView = findViewById(R.id.databinding_txt_painel)
+//        val botao1: Button = findViewById(R.id.databinding_btn_lambda)
+//        val botao2: Button = findViewById(R.id.databinding_btn_classe)
+//        val botao3: Button = findViewById(R.id.databinding_btn_metodo)
 
         // Acessando os métodos dos objetos
-        painel.text = getString(R.string.app_name)
-        painel.text = "${painel.text}\n ----\n\n"
+//        painel.text = getString(R.string.app_name)
+//        painel.text = "${painel.text}\n ----\n\n"
+
+        // 2. Usando data binding
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_data_binding)
+
+        // Acessando os métodos dos objetos
+        mBinding.databindingTxtPainel.text = getString(R.string.app_name)
+        mBinding.databindingTxtPainel.text = "${mBinding.databindingTxtPainel.text}\n ----\n\n"
 
         // Adicionando um evento ao botão
 
@@ -43,11 +56,31 @@ class DataBindingActivity : AppCompatActivity() {
         //      botão e pelo teclado)
 
         // 1. Cadastrando evento com função lambda
-        botao.setOnClickListener {
-            Toast.makeText(this, "Oi", Toast.LENGTH_SHORT).show()
-            painel.text = "Oi!"
+        mBinding.databindingBtnLambda.setOnClickListener {
+            Toast.makeText(this, "Lambda", Toast.LENGTH_SHORT).show()
+            mBinding.databindingTxtPainel.text = "Executou lambda"
         }
 
+        // 2. Cadastrando a própria classe para tratar o evento
+        //  - Classe precisa implementar a interface do listener
+        mBinding.databindingBtnClasse.setOnClickListener(this)
+
+        // 3. Cadastrando um método da classe
+        // - Passar o endereço do método que será executado - mesma assinatura da interface
+        // - Somente se o listener tiver um método
+        mBinding.databindingBtnMetodo.setOnClickListener(::onClickMetodo)
+
     }
+
+    override fun onClick(v: View?) {
+        Toast.makeText(this, "Listener da Classe", Toast.LENGTH_SHORT).show()
+        mBinding.databindingTxtPainel.text = "Executou Classe"
+    }
+
+    private fun onClickMetodo(v: View?) {
+        Toast.makeText(this, "Listener do Método", Toast.LENGTH_SHORT).show()
+        mBinding.databindingTxtPainel.text = "Executou Metodo"
+    }
+
 
 }
