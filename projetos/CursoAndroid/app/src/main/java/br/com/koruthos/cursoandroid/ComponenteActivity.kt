540dc72df291a036efcf3476f1302ce30e3b96bc
Chauã Queirolo <chaua@users.noreply.github.com>
2024-05-10 22:25:04 -0300

    :sparkles: Inclusão dos exemplos de componentes (com erro)

--- projetos/CursoAndroid/app/src/main/java/br/com/koruthos/cursoandroid/ComponenteActivity.kt
+++ projetos/CursoAndroid/app/src/main/java/br/com/koruthos/cursoandroid/ComponenteActivity.kt
@@ -1,13 +1,16 @@
 package br.com.koruthos.cursoandroid
 
 import android.annotation.SuppressLint
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import br.com.koruthos.cursoandroid.databinding.ActivityComponenteBinding
 import br.com.koruthos.cursoandroid.databinding.ActivityDataBindingBinding
@@ -19,6 +22,26 @@ class ComponenteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_componente)
+
+        // TextView: Eventos
+        mBinding.componenteTxtMensagem.setOnClickListener {
+            Toast.makeText(this, "Clicou TextView!", Toast.LENGTH_SHORT).show()
+            mBinding.componenteTxtMensagem.setTextColor(ContextCompat.getColor(this, R.color.green_800))
+        }
+
+        // InputEditText: Evento do teclado
+        mBinding.componenteEdtSenha.setOnEditorActionListener { v, actionId, event ->
+
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                if (mBinding.componenteEdtSenha.text.toString() == "123") {
+                    Toast.makeText(this, "Acertou", Toast.LENGTH_SHORT).show()
+                } else {
+                    mBinding.componenteEdtSenha.error = "Senha inválida!"
+                }
+                true
+            }
+            false
+        }
 
 
     }
