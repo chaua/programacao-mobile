c0c1610370290116b8f60e2689ac021a21777f7d
Chauã Queirolo <chaua@users.noreply.github.com>
2024-09-13 20:21:36 -0300

    :tada: Importação inicial

---
+++ projetos/CursoAndroid/app/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "br.com.koruthos.cursoandroid"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "br.com.koruthos.cursoandroid"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0.1.1"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+
+}
